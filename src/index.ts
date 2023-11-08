06fd46b6b5389ea04ef571733b33ffd11bdb795c
ChenQiang <1027869582@qq.com>
2023-11-08 14:22:26 +0800

    feat: 增加wasm打包

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@
  * @Author: ChenQiang
  * @Date: 2023-11-03 11:35:27
  * @LastEditors: ChenQiang
- * @LastEditTime: 2023-11-03 17:30:24
+ * @LastEditTime: 2023-11-08 14:19:51
  * @FilePath: \src\index.ts
  */
-export { Printer268 } from "./printer/impl/Printer268"
+// import { Printer268 } from "./printer/impl/Printer268"
+export function testFunc():void {
+  console.log('test')
+}
 const a = 'hi'
 console.log('test index.ts')
 console.log(a)
 console.log('test index.ts')
+// new Printer268().print()
--- src/printer/impl/Printer268.ts
+++ src/printer/impl/Printer268.ts
@@ -3,16 +3,16 @@
  * @Author: ChenQiang
  * @Date: 2023-11-03 14:35:16
  * @LastEditors: ChenQiang
- * @LastEditTime: 2023-11-03 17:30:36
+ * @LastEditTime: 2023-11-08 11:24:56
  * @FilePath: \src\printer\impl\Printer268.ts
  */
 
 import { Printer } from "../Printer";
 
 export class Printer268 implements Printer {
-  print() {
-    return new Promise<boolean>((resolve, reject) => {
-      return resolve(true);
+  print(): Promise<boolean> {
+    return new Promise<boolean>((resRej, reject) => {
+      return Promise.resolve(true);
     });
   };
 }
